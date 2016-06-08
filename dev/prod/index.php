dcd2865e2fe72d767bd523c73a857e8d355ca73a
Jorge Cortés <jorge.ecro@gmail.com>
2016-06-08 15:38:32 -0500

    First commit
    Work files initialization

---
+++ dev/prod/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="icon" href="favicon.png" sizes="32x32" type="image/png">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/jquery-1.11.3.min.js"></script>
+	<script src="js/actions.min.js"></script>
+	<!-- FONT AWESOME -->
+	<link href="extra/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+<div class="main-container"></div>
+	
+</body>
+</html>
