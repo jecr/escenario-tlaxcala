aeb8314c3608826c422e5dbfb43a4c95487cb1e9
Jorge Cortés <jorge.ecro@gmail.com>
2016-07-28 11:52:46 -0500

    Starter pack

--- dev/prod/index.php
+++ dev/prod/index.php
@@ -10,11 +10,25 @@
 	<script src="js/actions.min.js"></script>
 	<!-- FONT AWESOME -->
 	<link href="extra/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
 
-<div class="main-container"></div>
+<div class="main-container">
+	<div class="wrap main-menu">
+		<ul>
+			<li><a href="#columnas">COLUMNAS</a></li><li>
+			<a href="#radio">RADIO</a></li><li>
+			<a href="#noticias">NOTICIAS</a></li><li class="logo-et">
+			<a href="#inicio">INICIO</a></li><li>
+			<a href="#nosotros">NOSOTROS</a></li><li>
+			<a href="#acciones">ACCIONES</a></li><li>
+			<a href="#galería">GALERÍA</a></li>
+		</ul>
+		<div class="logo-back"></div>
+	</div>
+</div>
 	
 </body>
 </html>
